907a51223a2bdecec97ce8fbad40a695cb9d2f39
Spuc <jonasbylling@gmail.com>
2014-01-06 17:46:45 +0100

    Opg1 done

---
+++ JavaBean/Opg1/AgeErrorEvent.java
@@ -0,0 +1,12 @@
+package Opg1;
+
+import java.util.EventObject;
+
+public class AgeErrorEvent extends EventObject {
+
+	public AgeErrorEvent(Object eventSource) {
+		super(eventSource);
+		// TODO Auto-generated constructor stub
+	}
+
+}
